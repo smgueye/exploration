69d8a032c1b9969a1e347feaa9b8b7b3e89da53d
Mouhamed <mouhamed.gueye-ext@isocel.leclerc>
2025-08-14 00:48:36 +0200

    Extract sub class method fixed.

---
+++ src/main/java/io/mgueye/oop/code_smell/extract_sub_classes/level1/ElectricVehicle.java
@@ -0,0 +1,30 @@
+package io.mgueye.oop.code_smell.extract_sub_classes.level1;
+
+public class ElectricVehicle extends Vehicle {
+
+  private final double batteryCapacity;
+  private double currentCharge;
+
+  public ElectricVehicle(String make, String model, int year, double batteryCapacity, double currentCharge) {
+    super(make, model, year);
+    this.batteryCapacity = batteryCapacity;
+    this.currentCharge = currentCharge;
+  }
+
+  public void chargeBattery(double kWh) {
+    if (batteryCapacity == 0) {
+      System.out.println("This vehicle has no battery!");
+      return;
+    }
+    currentCharge = Math.min(currentCharge + kWh, batteryCapacity);
+    System.out.println("Charged battery to " + currentCharge + " kWh");
+  }
+
+  public double getBatteryPercentage() {
+    if (batteryCapacity == 0) {
+      System.out.println("This vehicle has no battery!");
+      return 0;
+    }
+    return (currentCharge / batteryCapacity) * 100;
+  }
+}
---
+++ src/main/java/io/mgueye/oop/code_smell/extract_sub_classes/level1/Vehicle.java
@@ -0,0 +1,18 @@
+package io.mgueye.oop.code_smell.extract_sub_classes.level1;
+
+public class Vehicle {
+  private String make;
+  private String model;
+  private int year;
+
+  public Vehicle(String make, String model, int year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  // Normal behavior
+  public void drive() {
+    System.out.println("Driving " + make + " " + model);
+  }
+}
