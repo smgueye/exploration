d64544f5b51e9e04a14cddae1bd84c4f5101b7b3
Mouhamed <mouhamed.gueye-ext@isocel.leclerc>
2025-08-23 23:53:59 +0200

    Code smell(Primitive obsession) : Employee
    AFTER REVIEWER COMMENT.

--- src/main/java/io/mgueye/oop/code_smell/primitive_obession/employee/Init.java
+++ src/main/java/io/mgueye/oop/code_smell/primitive_obession/employee/Init.java
@@ -1,66 +1,41 @@
 package io.mgueye.oop.code_smell.primitive_obession.employee;
 
+import lombok.Getter;
+
 public class Init {
 
   public static void main(String[] args) {
   }
 
-  public abstract class Employee {
+  public class Employee {
     private final String name;
+    private final EmployeeType type;
 
-    public Employee(String name) {
+    public Employee(String name, EmployeeType type) {
       this.name = name;
+      this.type = type;
     }
 
-    public abstract int payAmount();
-    public abstract String getTitle();
-  }
-
-  public class Engineer extends Employee {
-    public Engineer(String name, int type) {
-      super(name);
-    }
-
-    @Override
     public int payAmount() {
-      return 3000;
+      return type.getPay();
     }
-
-    @Override
     public String getTitle() {
-      return "Engineer";
+      return type.getTitle();
     }
   }
 
-  public class Manager extends Employee {
-    public Manager(String name) {
-      super(name);
+  @Getter
+  public static class EmployeeType {
+    private final String title;
+    private final int pay;
+
+    public EmployeeType(String title, int pay) {
+      this.title = title;
+      this.pay = pay;
     }
 
-    @Override
-    public int payAmount() {
-      return 5000;
-    }
-
-    @Override
-    public String getTitle() {
-      return "Manager";
-    }
-  }
-
-  public class Salesperson extends Employee {
-    public Salesperson(String name) {
-      super(name);
-    }
-
-    @Override
-    public int payAmount() {
-      return 4000;
-    }
-
-    @Override
-    public String getTitle() {
-      return "Salesperson";
-    }
+    public static final EmployeeType ENGINEER = new EmployeeType("Engineer", 3000);
+    public static final EmployeeType MANAGER = new EmployeeType("Manager", 5000);
+    public static final EmployeeType SALES = new EmployeeType("Salesperson", 4000);
   }
 }
